c2a9c84f8f2fc9fff1056fb4483b8ea2f5335b64
Exucounter <exucounter777@gmail.com>
2020-03-24 20:52:13 +0200

    Styling auth page

---
+++ js/password-input.js
@@ -0,0 +1,15 @@
+let passwordInput = document.querySelector(".input-container__input-password");
+let eyeButton = document.querySelector(".input-container__svg-container__show-btn");
+  
+alert(eyeButton);
+eyeButton.addEventListener('click', ()=>{
+  if(passwordInput.getAttribute('type') == 'password') {
+    passwordInput.removeAttribute('type');
+    passwordInputt.setAttribute('type', 'text');
+
+   } else {
+    passwordInput.removeAttribute('type');
+    passwordInput.setAttribute('type', 'password');
+
+   }
+});
