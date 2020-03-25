cd1778b7aeda5dc9e6f20d24971c49e99353c67b
Exucounter <exucounter777@gmail.com>
2020-03-25 23:23:49 +0200

    End styling

---
+++ js/slick-apartments.js
@@ -0,0 +1,9 @@
+$('.main-search-result__block__inner-container').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    dots: true,
+    dotsClass: 'main-search-result__block__pagination',
+    autoplaySpeed: 3000,
+    arrows: true
+  });
